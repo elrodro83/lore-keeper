3c9c5b6a6eda96f1d430bc358c23a0dbff94a7a5
rmerino <rmerino@avaya.com>
2015-05-13 10:53:36 -0300

    Staring anew

--- backend/src/main/java/ar/com/lorekeeper/server/bean/Geography.java
+++
@@ -1,5 +0,0 @@
-package ar.com.lorekeeper.server.bean;
-
-public class Geography {
-
-}
--- backend/src/main/java/ar/com/lorekeeper/server/dao/Dao.java
+++
@@ -1,9 +0,0 @@
-package ar.com.lorekeeper.server.dao;
-
-import ar.com.lorekeeper.server.bean.PersistentObject;
-
-public interface Dao {
-
-	PersistentObject find(Object id);
-
-}
